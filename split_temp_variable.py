bc9b0e643362285fb90bb2e075796aa9bb5880d6
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-02-18 10:33:30 -0800

    remove_assignment.. working

--- split_temp_variable.py
+++ split_temp_variable.py
@@ -1,18 +1,30 @@
-# by Kami Bigdely
-# Split temporary variable
 
-patty = 70 # [gr]
-pickle = 20 # [gr]
-tomatoes = 25 # [gr]
-lettuce = 15 # [gr]
-buns = 95 # [gr]
-sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes + 2 * lettuce
-                + 2 * buns)
-print("NY Burger Weight", sandwich_weight)
-kimchi = 30 # [gr]
-mayo = 5 # [gr]
-golden_fried_onion = 20 # [gr]
-sandwich_weight = (2 * patty + 4 * pickle + 3 * tomatoes 
-                + kimchi + mayo + golden_fried_onion + 2 * buns)
-print("Seoul Kimchi Burger Weight", sandwich_weight)
+"""This file prints the weight of sandwhich based on type"""
 
+
+class Sandwhich:
+    """Represents ingredients of a sandwhich"""
+
+    def __init__(self, patty, pickle, tomatoes, buns):
+        self.patty = patty
+        self.pickle = pickle
+        self.tomatoes = tomatoes
+        self.buns = buns
+
+    def ny_sandwhich(self, lettuce):
+        """Returns the weight of New York sandwhich"""
+
+        ny_sandwhich = (2 * self.patty + 4 * self.pickle + 3 * self.tomatoes + 2
+                        * lettuce + 2 * self.buns)
+        return print("NY Burger Weight", ny_sandwhich)
+
+    def kimchi_sandwhich(self, mayo, golden_onion, kimchi):
+        """Returns the weight of Kimchi sandwhich"""
+
+        kimchi_sandwhich = (2 * self.patty + 4 * self.pickle + 3 * self.tomatoes
+                            + kimchi + mayo + golden_onion + 2 * self.buns)
+        return print("Seoul Kimchi Burger Weight", kimchi_sandwhich)
+
+
+sandwhich_one = Sandwhich(70, 20, 15, 95).ny_sandwhich(25)
+sandwhich_two = Sandwhich(70, 20, 15, 95).kimchi_sandwhich(5, 20, 30)
